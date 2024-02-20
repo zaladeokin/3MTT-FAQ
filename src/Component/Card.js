34e84630f719beeb7009b532b2cec72e6cfee5f6
Zacchaeus Aladeokin <zaladeokin@gmail.com>
2024-02-20 16:46:01 +0100

    Auto close of detail card when another is open implemented

--- src/Component/Card.js
+++ src/Component/Card.js
@@ -1,8 +1,20 @@
 import '../css/Card.css'
 
 function Card({info, firstItem}) {
+    const handleClick= (e)=>{
+        let allDet= document.querySelectorAll("details");
+
+        allDet.forEach((det)=>{
+            det.removeAttribute('open');
+        });
+
+        let att= document.createAttribute("open");
+        att.value= true;
+        e.target.setAttributeNode(att)
+    }
+
     return(
-        <details open={firstItem}>
+        <details open={firstItem} onClick={handleClick}>
             <summary>{info.question}</summary>
             <p>{info.answer}</p>
           </details>
